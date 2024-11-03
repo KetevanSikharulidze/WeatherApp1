bb39207a37536184fe6fed4fd317993646fe346c
justKeth <sixaruliketi@gmail.com>
2024-11-03 23:40:03 +0400

    empty main

--- app/src/main/java/com/example/weatherapp1/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp1/MainActivity.kt
@@ -17,43 +17,17 @@ const val API_KEY = "30526069901d408fa5a182653240311"
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        binding.BtnGetWeather.setOnClickListener {
-            getResult("London")
-        }
-
     }
 
-    private fun getResult(name: String){
-        val url = "https://api.weatherapi.com/v1/current.json" +
-                "?key=$API_KEY&q=$name&aqi=no\n"
-        val queue = Volley.newRequestQueue(this) // ამითი ვაგზავნით მოთხოვნას
-        val stringRequest = StringRequest(Request.Method.GET,
-            url,
-            {
-                response ->
-                val obj = JSONObject(response)
-                val temp = obj.getJSONObject("current")
-                Log.d("MyLog","response: ${temp.getString("temp_c")}") //response
-            },
-            {
-                Log.d("MyLog",it.toString()) //error
-            }
-            )
-        queue.add(stringRequest) //აქ ვამატებთ ახალ მოთხოვნას
-    }
+
 }
